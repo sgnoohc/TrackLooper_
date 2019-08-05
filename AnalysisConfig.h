ac9e2a3dd5668651ebaeeb0c4a52176db37193ea
Philip Chang <sgnoohc@gmail.com>
2019-08-05 11:17:30 -0700

    update

---
+++ AnalysisConfig.h
@@ -0,0 +1,44 @@
+#ifndef AnalysisConfig_h
+#define AnalysisConfig_h
+
+#include "TString.h"
+#include "rooutil.h"
+#include "trktree.h"
+
+class AnalysisConfig
+{
+
+public:
+
+    // TString that holds the input file list (comma separated)
+    TString input_file_list_tstring;
+
+    // TString that holds the name of the TTree to open for each input files
+    TString input_tree_name;
+
+    // Output TFile
+    TFile* output_tfile;
+
+    // Number of events to loop over
+    int n_events;
+
+    // Debug boolean
+    bool debug;
+
+    // TChain that holds the input TTree's
+    TChain* events_tchain;
+
+    // Custom Looper object to facilitate looping over many files
+    RooUtil::Looper<trktree> looper;
+
+    // Custom Cutflow framework
+    RooUtil::Cutflow cutflow;
+
+    // Custom Histograms object compatible with RooUtil::Cutflow framework
+    RooUtil::Histograms histograms;
+
+};
+
+extern AnalysisConfig ana;
+
+#endif
