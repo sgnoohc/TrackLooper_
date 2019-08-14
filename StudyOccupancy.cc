e241b5f7dbeb3f3801b702905783315d915b4834
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-08-13 18:06:51 -0700

    More histograms

--- StudyOccupancy.cc
+++ StudyOccupancy.cc
@@ -8,18 +8,30 @@ StudyOccupancy::StudyOccupancy(const char *studyName)
 void StudyOccupancy::bookStudy()
 {
 
-    ana.histograms.addHistogram(TString::Format("occupancy_in_barrel"),1000,0,100,[&](){return averageOccupancyInBarrel;});
+    //adding vector histograms for dumping occupancy numbers
 
-    ana.histograms.addHistogram(TString::Format("occupancy_in_endcap"),1000,0,100,[&](){return averageOccupancyInEndcap;});
+    ana.histograms.addVecHistogram(TString::Format("occupancy_in_barrel"),1000,0,100,[&](){return occupancyInBarrel;})
+
+    ana.histograms.addHistogram(TString::Format("average_occupancy_in_barrel"),1000,0,100,[&](){return averageOccupancyInBarrel;});
+
+    ana.histograms.addVecHistogram(TString::Format("occupancy_in_endcap"),1000,0,100,[&](){return occupancyInEndcap;});
+
+    ana.histograms.addHistogram(TString::Format("average_occupancy_in_endcap"),1000,0,100,[&](){return averageOccupancyInEndcap;});
 
     for(int i = 0; i<6; i++)
     {
-      ana.histograms.addHistogram(TString::Format("occupancy_in_layer_%d",i+1),1000,0,100,[&,i](){return averageLayerOccupancy[i];});
+      ana.histograms.addHistogram(TString::Format("average_occupancy_in_layer_%d",i+1),1000,0,100,[&,i](){return averageLayerOccupancy[i];});
 
-      ana.histograms.addHistogram(TString::Format("occupancy_in_barrel_for_layer_%d",i+1),1000,0,100,[&,i](){return averageBarrelLayerOccupancy[i];});
+      ana.histograms.addVecHistogram(TString::Format("occupancy_in_layer_%d",i+1),1000,0,100,[&,i](){return LayerOccupancy.at(i);});
 
+      ana.histograms.addHistogram(TString::Format("average_occupancy_in_barrel_for_layer_%d",i+1),1000,0,100,[&,i](){return averageBarrelLayerOccupancy[i];});
 
-      ana.histograms.addHistogram(TString::Format("occupancy_in_endcap_for_layer_%d",i+1),1000,0,100,[&,i](){return averageEndcapLayerOccupancy[i];});
+      ana.histograms.addVecHistogram(TString::Format("occupancy_in_barrel_for_layer_%d",i+1),1000,0,100,[&,i](){return BarrelLayerOccupancy.at(i);});
+
+      ana.histograms.addHistogram(TString::Format("average_occupancy_in_endcap_for_layer_%d",i+1),1000,0,100,[&,i](){return averageEndcapLayerOccupancy[i];});
+
+      ana.histograms.addVecHistogram(TString::Format("occupancy_in_endcap_for_layer_%d",i+1),1000,0,100,[&,i](){return EndcapLayerOccupancy.at(i);});
+
     }
 
 }
@@ -40,6 +52,21 @@ void StudyOccupancy::doStudy(SDL::Event& event, std::vector<std::tuple<unsigned 
     averageEndcapLayerOccupancy = {0,0,0,0,0,0};
     std::vector<int> nEndcapLayerModules = {0,0,0,0,0,0};
     int nBarrelModules = 0, nEndcapModules = 0;
+
+    occupancyInBarrel.clear();
+    occupancyInEndcap.clear();
+
+    //Layer occupancy
+    LayerOccupancy.clear();
+    BarrelLayerOccupancy.clear();
+    EndcapLayerOccupancy.clear();
+    //setting up sub-vectors for the barrel and endcap layer occupancy
+    for(int i = 1; i<=6;i++)
+    {
+        BarrelLayerOccupancy.push_back(std::vector<float>);
+        EndcapLayerOccupancy.push_back(std::vector<float>);
+    }
+
     //To get the occupancy, iterate through the modules and get the length
     //of the vector of pointers containing the hits on that module
     for(auto &module:moduleList)
@@ -51,16 +78,25 @@ void StudyOccupancy::doStudy(SDL::Event& event, std::vector<std::tuple<unsigned 
         averageOccupancyInBarrel += (module->getHitPtrs()).size();
         nBarrelModules ++;
 
+        occupancyInBarrel.push_back((module->getHitPtrs()).size());
+
         averageBarrelLayerOccupancy.at(module->layer()-1) += (module->getHitPtrs()).size();
         nBarrelLayerModules.at(module->layer()-1) ++;
+
+        BarrelLayerOccupancy.at(module->layer()-1).push_back((module->getHitPtrs().size()));
+
       }
       else if(module->subdet() == SDL::Module::Endcap) //endcap module
       {
         averageOccupancyInEndcap += (module->getHitPtrs()).size();
         nEndcapModules ++;
 
+        occupancyInEndcap.push_back((module->getHitPtrs()).size());
+
         averageEndcapLayerOccupancy.at(module->layer()-1) += (module->getHitPtrs()).size();
         nEndcapLayerModules.at(module->layer()-1) ++;
+
+        EndcapLayerOccupancy.at(module->layer()-1).push_back((module->getHitPtrs().size()));
       }
     }
 
--- StudyOccupancy.h
+++ StudyOccupancy.h
@@ -17,11 +17,17 @@ class StudyOccupancy : public Study
 public:
   const char * studyname;
 
+  std::vector<float> occupancyInBarrel;
+  std::vector<float> occupancyInEndcap;
   float averageOccupancyInBarrel;
   float averageOccupancyInEndcap;
   std::vector<float> averageLayerOccupancy;
   std::vector<float> averageBarrelLayerOccupancy;
   std::vector<float> averageEndcapLayerOccupancy;
+
+  std::vector<std::vector<float>> LayerOccupancy;
+  std::vector<std::vector<float>> BarrelLayerOccupancy;
+  std::vector<std::vector<float>> EndcapLayerOccupancy;
 
   StudyOccupancy(const char * studyName);
   virtual void bookStudy();
