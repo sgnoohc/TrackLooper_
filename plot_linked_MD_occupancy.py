99751327439ddba5fb448d92af2320cf6fb629cb
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-08-26 19:20:43 -0700

    updated plotter scripts

--- plot_linked_MD_occupancy.py
+++ plot_linked_MD_occupancy.py
@@ -8,18 +8,25 @@ filename = "debug.root"
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 
-xaxis_range = [0,25] #SUBJECT TO CHANGE
+xaxis_range = [0,50] #SUBJECT TO CHANGE
+xaxis_range_average = [0,25]
 
 def plot_occupancy(hist,prefix):
-    global xaxis_range
+    global xaxis_range,xaxis_range_average
     filename_prefix = prefix.replace(" ","_")
     filename_prefix = "/home/users/bsathian/public_html/SDL/"+filename_prefix
+    xaxis_plot = None
+    if "average" not in prefix:
+        xaxis_plot = xaxis_range
+    else:
+        xaxis_plot = xaxis_range_average
+
     ply.plot_hist(
         bgs = [hist],
         legend_labels = [prefix],
         options = {
         "output_name":filename_prefix+".pdf",
-        "xaxis_range":xaxis_range,
+        "xaxis_range":xaxis_plot,
         "xaxis_label":prefix,
         "title":prefix,
         "legend_percentageinbox":False,
@@ -30,7 +37,6 @@ def plot_occupancy(hist,prefix):
 
 f = r.TFile(filename)
 layer_average_occupancy_hists = []
-layer_occupancy_hists = []
 layer_barrel_average_occupancy_hists = []
 layer_barrel_occupancy_hists = []
 layer_endcap_average_occupancy_hists = []
@@ -38,25 +44,49 @@ layer_endcap_occupancy_hists = []
 ring_endcap_average_occupancy_hists = []
 ring_endcap_occupancy_hists = []
 
+linked_average_modules_in_barrel_layer = []
+linked_average_modules_in_endcap_layer = []
+linked_average_modules_in_endcap_ring = []
+
+linked_modules_in_barrel_layer = []
+linked_modules_in_endcap_layer = []
+linked_modules_in_endcap_ring = []
+
 for i in range(1,7):
     layer_average_occupancy_hists.append(f.Get("Root__average_Linked_MD_occupancy_in_layer_"+str(i)))
     layer_barrel_average_occupancy_hists.append(f.Get("Root__average_Linked_MD_occupancy_in_barrel_for_layer_"+str(i)))
     layer_endcap_average_occupancy_hists.append(f.Get("Root__average_Linked_MD_occupancy_in_endcap_for_layer_"+str(i)))
 
-    layer_occupancy_hists.append(f.Get("Root__Linked_MD_occupancy_in_layer_"+str(i)))
     layer_barrel_occupancy_hists.append(f.Get("Root__Linked_MD_occupancy_in_barrel_for_layer_"+str(i)))
     layer_endcap_occupancy_hists.append(f.Get("Root__Linked_MD_occupancy_in_endcap_for_layer_"+str(i)))
+
+    linked_average_modules_in_barrel_layer.append(f.Get("Root__average_number_of_Linked_modules_in_barrel_for_layer_"+str(i)))
+    linked_average_modules_in_endcap_layer.append(f.Get("Root__average_number_of_Linked_modules_in_endcap_for_layer_"+str(i)))
+    linked_modules_in_barrel_layer.append(f.Get("Root__number_of_Linked_modules_in_barrel_for_layer_"+str(i)))
+    linked_modules_in_endcap_layer.append(f.Get("Root__number_of_Linked_modules_in_endcap_for_layer_"+str(i)))
+
 
 for i in range(1,16):
     ring_endcap_average_occupancy_hists.append(f.Get("Root__average_Linked_MD_occupancy_in_endcap_for_ring_"+str(i)))
     ring_endcap_occupancy_hists.append(f.Get("Root__Linked_MD_occupancy_in_endcap_for_ring_"+str(i)))
 
+    linked_average_modules_in_endcap_ring.append(f.Get("Root__average_number_of_Linked_modules_in_endcap_for_ring_"+str(i)))
+    linked_modules_in_endcap_ring.append(f.Get("Root__number_of_Linked_modules_in_endcap_for_ring_"+str(i)))
 
 
+
+
+barrel_occupancy_hist = f.Get("Root__Linked_MD_occupancy_in_barrel")
 endcap_occupancy_hist = f.Get("Root__Linked_MD_occupancy_in_endcap")
 
 barrel_average_occupancy_hist = f.Get("Root__average_Linked_MD_occupancy_in_barrel")
 endcap_average_occupancy_hist = f.Get("Root__average_Linked_MD_occupancy_in_endcap")
+
+barrel_average_linked_modules_hist = f.Get("Root__average_number_of_Linked_modules_in_barrel")
+endcap_average_linked_modules_hist = f.Get("Root__average_number_of_Linked_modules_in_endcap")
+
+barrel_linked_module_hist = f.Get("Root__number_of_Linked_modules_in_barrel")
+endcap_linked_module_hist = f.Get("Root__number_of_Linked_modules_in_endcap")
 
 plot_occupancy(barrel_occupancy_hist,"barrel mini-doublet occupancy")
 plot_occupancy(endcap_occupancy_hist,"endcap mini-doublet occupancy")
@@ -64,23 +94,40 @@ plot_occupancy(endcap_occupancy_hist,"endcap mini-doublet occupancy")
 plot_occupancy(barrel_average_occupancy_hist,"barrel average mini-doublet occupancy")
 plot_occupancy(endcap_average_occupancy_hist,"endcap average mini-doublet occupancy")
 
+plot_occupancy(barrel_average_linked_modules_hist,"average number of linked modules in barrel")
+plot_occupancy(endcap_average_linked_modules_hist,"average number of linked modules in endcap")
 
-for i in range(len(layer_occupancy_hists)):
-    plot_occupancy(layer_occupancy_hists[i],"Linked Mini-doublet Occupancy for layer "+str(i+1))
+plot_occupancy(barrel_linked_modules_hist,"number of linked modules in barrel")
+plot_occupancy(endcap_linked_modules_hist,"number of linked modules in endcap")
+
+
+for i in range(len(layer_barrel_occupancy_hists)):
     plot_occupancy(layer_barrel_occupancy_hists[i],"Barrel Linked Mini-doublet Occupancy for layer "+str(i+1))
     if i != 5:
         plot_occupancy(layer_endcap_occupancy_hists[i],"Endcap Linked mini-doublet Occupancy for layer "+str(i+1))
 
 
 
-for i in range(len(layer_average_occupancy_hists)):
-    plot_occupancy(layer_average_occupancy_hists[i],"Average Linked mini-doublet occupancy for layer "+str(i+1))
+
+for i in range(len(layer_barrel_average_occupancy_hists)):
     plot_occupancy(layer_barrel_average_occupancy_hists[i],"Average barrel Linked mini-doublet occupancy for layer "+str(i+1))
+    if i!=5 :
+        plot_occupancy(linked_modules_in_barrel_layer[i],"number of linked modules in barrel layer "+str(i+1))
+        plot_occupancy(linked_average_modules_in_barrel_layer[i],"average number of linked modules in barrel layer "+str(i+1))
     if i!= 5:
         plot_occupancy(layer_endcap_average_occupancy_hists[i],"Average endcap Linked mini-doublet occupancy for layer "+str(i+1))
+    if i<4:
+        plot_occupancy(linked_average_modules_in_endcap_layer[i],"average number of linked modules in endcap layer "+str(i+1))
+        plot_occupancy(linked_modules_in_endcap_layer[i],"number of linked modules in endcap layer "+str(i+1))
+
 
 for i in range(len(ring_endcap_average_occupancy_hists)):
     plot_occupancy(ring_endcap_average_occupancy_hists[i],"Average endcap mini-doublet occupancy in ring "+str(i+1))
     plot_occupancy(ring_endcap_occupancy_hists[i],"Endcap mini-doublet occupancy in ring "+str(i+1))
+    if i< 14:
+        plot_occupancy(linked_modules_in_endcap_ring[i],"number of linked modules in endcap ring "+str(i+1))
+        plot_occupancy(linked_average_modules_in_endcap_ring[i],"number of linked modules in endcap ring "+str(i+1))
+
+
 
 
